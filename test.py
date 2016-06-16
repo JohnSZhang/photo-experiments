8adb79e08c4ede6154a5b543a7979356fd45856b
John Zhang <john.shimin.zhang@gmail.com>
2016-06-16 20:03:23 +0700

    add a couple of color shifting images

--- test.py
+++ test.py
@@ -1,24 +1,11 @@
 from PIL import Image
+from lib.latergram import Latergram
 
 img = Image.open('photo/peacock.jpg')
 
-# print(img.format, img.size, img.mode)
-img.point(lambda a: print(a))
-#
-img.show();
-imgSource = img.split()
-
-R, G, B = 0, 1, 2
-
-red = imgSource[R].point(lambda i: min(i, 255))
-
-green = imgSource[G].point(lambda i: min(i, 255))
-
-blue = imgSource[B].point(lambda i: min(i + 55, 255))
-
-# paste the processed band back, but only where red was < 100
-# source[G].paste(out, None, mask)
-
-# build a new multiband image
-im = Image.merge(img.mode, [red, green, blue])
-im.show()
+latergram = Latergram(img)
+latergram.mtint(1.2)
+latergram.show()
+latergram.setOriginalPhoto()
+latergram.ftint(20)
+latergram.show()
