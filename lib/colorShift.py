0bfbf41ac17510d6280624384ac684be990b62fc
John Zhang <john.shimin.zhang@gmail.com>
2016-06-17 19:35:01 +0700

    refactor colorshift a little

--- lib/colorShift.py
+++ lib/colorShift.py
@@ -1,57 +1,59 @@
 from PIL import Image
 
 class ColorShift:
+
+    # Update current photo as an image made up of the supplied rgb bands
+    def updatePhotoFromBands(self, bands):
+        self.setPhoto(Image.merge(self.photo.mode, bands))
+
+    # get rgb bands of current photo
+    def currentBands(self):
+        return self.getPhoto().split()
+
+    # take an array of r g b values, apply to each of the rgb bands and set as current photo
+    def updateFixedChange(self, deltas):
+        imgSource = self.currentBands()
+        R, G, B = 0, 1, 2
+        red = imgSource[R].point(lambda i: max(0, min(i + deltas[R], 255)))
+        green = imgSource[G].point(lambda i: max(0, min(i + deltas[G], 255)))
+        blue = imgSource[B].point(lambda i: max(0, min(i + deltas[B], 255)))
+
+        self.updatePhotoFromBands([red, green, blue])
+
+    # take a series of multipliers for each of the RGB bands and applu
+    def updateLinearChange(self, multipliers):
+        imgSource = self.currentBands()
+        R, G, B = 0, 1, 2
+        red = imgSource[R].point(lambda i: max(0, min(i  * multipliers[R], 255)))
+        green = imgSource[G].point(lambda i: max(0, min(i * multipliers[G], 255)))
+        blue = imgSource[B].point(lambda i: max(0, min(i * multipliers[B], 255)))
+
+        self.updatePhotoFromBands([red, green, blue])
+
     # add or subtract fixed delta to shift the photo red / blue
     def fwarmth(self, delta):
         if (abs(delta) > 100):
             raise Exception('Please enter a warmth delta between -100 and 100')
 
-        imgSource = self.getPhoto().split();
-        R, G, B = 0, 1, 2
-
-        red = imgSource[R].point(lambda i: max(0, min(i + delta, 255)))
-        green = imgSource[G]
-        blue = imgSource[B].point(lambda i: max(0, min(i - delta, 255)))
-
-        self.setPhoto(Image.merge(self.photo.mode, [red, green, blue]))
-
-    # shifts the warmth of a photo by a multiplier between 0.5 and 2.0
-    def mwarmth(self, multiplier):
-        if (multiplier < 0.5 or multiplier > 2.0):
-            raise Exception('Please enter a multipler between 0.5 and 2.0')
-        imgSource = self.getPhoto().split();
-        R, G, B = 0, 1, 2
-
-        red = imgSource[R].point(lambda i: max(0, min(i * multiplier, 255)))
-        green = imgSource[G]
-        blue = imgSource[B].point(lambda i: max(0, min(i * (1 / multiplier), 255)))
-
-        self.photo = Image.merge(self.photo.mode, [red, green, blue])
+        self.updateFixedChange([delta, 0, delta * -1])
 
     # shifts tint of photo for a fixed amount between -100 and 100
     def ftint(self, delta):
         if (abs(delta) > 100):
             raise Exception('Please enter a tint delta between -100 and 100')
 
-        imgSource = self.getPhoto().split();
-        R, G, B = 0, 1, 2
+        self.updateFixedChange([0, delta, 0])
 
-        red = imgSource[R]
-        green = imgSource[G].point(lambda i: max(0, min(i + delta, 255)))
-        blue = imgSource[B]
+    # shifts the warmth of a photo by a multiplier between 0.3 and 2.0
+    def mwarmth(self, multiplier):
+        if (multiplier < 0.3 or multiplier > 2.0):
+            raise Exception('Please enter a multipler between 0.3 and 2.0')
 
-        self.setPhoto(Image.merge(self.photo.mode, [red, green, blue]))
+        self.updateLinearChange([multiplier, 1, (1 / multiplier)])
 
     # shifts tint of photo for a multiplier between 0.5 and 2
     def mtint(self, multiplier):
         if (multiplier < 0.5 or multiplier > 2.0):
             raise Exception('Please enter a multipler between 0.5 and 2.0')
 
-        imgSource = self.getPhoto().split();
-        R, G, B = 0, 1, 2
-
-        red = imgSource[R]
-        green = imgSource[G].point(lambda i: max(0, min(i * multiplier, 255)))
-        blue = imgSource[B]
-
-        self.setPhoto(Image.merge(self.photo.mode, [red, green, blue]))
+        self.updateLinearChange([1, multiplier, 1 ])
--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 from PIL import Image
 from lib.latergram import Latergram
 
-img = Image.open('photo/whiteadjust.jpg')
+img = Image.open('photo/kyoto.jpg')
 
 latergram = Latergram(img)
-latergram.findBrightest()
+# latergram.show()
+# latergram.fwarmth(-50)
+# latergram.show()
+# latergram.setOriginalPhoto()
+# latergram.mwarmth(1.6)
+# latergram.show()
+# latergram.setOriginalPhoto()
+# latergram.mwarmth(0.4)
+# latergram.show()
+# latergram.findBrightest()
